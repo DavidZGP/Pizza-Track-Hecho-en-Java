67aa23672429b552eec8aba9fb3f67b7ceb76f9e
DavidZGP <david.piedrahitap@est.iudigital.edu.co>
2026-05-17 17:59:48 -0500

    Update PilaPizza.java
    Se agregan métodos peek e isEmpty y  se implementa del método clear para limpiar la pila. y se Corrigen algunos errores minimos

--- src/PilaPizza.java
+++ src/PilaPizza.java
@@ -4,24 +4,37 @@ public class PilaPizza {
 
     public PilaPizza() {
         tope = null;
-    }    
-     public void push(Pizza pizza) {
+    }
 
+    public void push(Pizza pizza) {
         NodoPizza nuevo = new NodoPizza(pizza);
-
         nuevo.siguiente = tope;
         tope = nuevo;
     }
 
     public Pizza pop() {
-
         if (isEmpty()) {
             return null;
         }
 
         Pizza pizza = tope.dato;
-
         tope = tope.siguiente;
-
         return pizza;
     }
+
+    public Pizza peek() {
+        if (isEmpty()) {
+            return null;
+        }
+
+        return tope.dato;
+    }
+
+    public boolean isEmpty() {
+        return tope == null;
+    }
+
+    public void clear() {
+        tope = null;
+    }
+}
