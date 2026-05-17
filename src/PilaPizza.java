80e690fbc06ce4250aee702bed2e02b8ea992523
DavidZGP <david.piedrahitap@est.iudigital.edu.co>
2026-05-17 17:52:13 -0500

    Update PilaPizza.java
    Implementación del método push en la pila  y aplicación del método pop

--- src/PilaPizza.java
+++ src/PilaPizza.java
@@ -4,4 +4,24 @@ public class PilaPizza {
 
     public PilaPizza() {
         tope = null;
+    }    
+     public void push(Pizza pizza) {
+
+        NodoPizza nuevo = new NodoPizza(pizza);
+
+        nuevo.siguiente = tope;
+        tope = nuevo;
+    }
+
+    public Pizza pop() {
+
+        if (isEmpty()) {
+            return null;
+        }
+
+        Pizza pizza = tope.dato;
+
+        tope = tope.siguiente;
+
+        return pizza;
     }
