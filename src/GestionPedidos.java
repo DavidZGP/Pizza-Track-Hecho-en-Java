df1de281ff25d40e5af42c11819dcabc9abf1c0a
DavidZGP <david.piedrahitap@est.iudigital.edu.co>
2026-05-17 18:55:14 -0500

    Update GestionPedidos.java
    Creación del menú interactivo en consola y Validación de entradas y pruebas finales del sistema.   Arreglo de Pruebas Menores

--- src/GestionPedidos.java
+++ src/GestionPedidos.java
@@ -62,3 +62,93 @@ public class GestionPedidos {
         System.out.println("Pedido actual:");
         System.out.println(pilaPrincipal.peek());
     }
+
+     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        GestionPedidos sistema = new GestionPedidos();
+
+        int opcion;
+
+        do {
+
+            System.out.println("\n===== PIZZA TRACK =====");
+            System.out.println("1. Registrar Pizza");
+            System.out.println("2. Deshacer (Undo)");
+            System.out.println("3. Rehacer (Redo)");
+            System.out.println("4. Mostrar Pedido Actual");
+            System.out.println("0. Salir");
+
+            System.out.print("Seleccione una opcion: ");
+            opcion = leerEntero(sc);
+
+            switch (opcion) {
+
+                case 1:
+
+                    System.out.print("Nombre de la pizza: ");
+                    String nombre = sc.nextLine();
+
+                    String[] ingredientes = new String[3];
+
+                    for (int i = 0; i < 3; i++) {
+
+                        System.out.print("Ingrediente " + (i + 1) + ": ");
+                        ingredientes[i] = sc.nextLine();
+                    }
+
+                    Pizza pizza = new Pizza(nombre, ingredientes);
+
+                    sistema.registrarPizza(pizza);
+
+                    break;
+
+                case 2:
+
+                    sistema.deshacer();
+
+                    break;
+
+                case 3:
+
+                    sistema.rehacer();
+
+                    break;
+
+                case 4:
+
+                    sistema.mostrarPedidoActual();
+
+                    break;
+
+                case 0:
+
+                    System.out.println("Saliendo del sistema...");
+
+                    break;
+
+                default:
+
+                    System.out.println("Opcion invalida.");
+            }
+
+        } while (opcion != 0);
+
+        sc.close();
+    }
+
+    public static int leerEntero(Scanner sc) {
+
+        while (!sc.hasNextInt()) {
+
+            System.out.print("Ingrese un numero valido: ");
+            sc.next();
+        }
+
+        int numero = sc.nextInt();
+        sc.nextLine();
+
+        return numero;
+    }
+}
