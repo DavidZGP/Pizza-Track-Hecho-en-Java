14c8258f47d52bee0f4d6baa562bc278d47109fb
DavidZGP <david.piedrahitap@est.iudigital.edu.co>
2026-05-17 18:21:00 -0500

    Update GestionPedidos.java
    Implementación del registro de pizzas y Desarrollo de la función deshacer (Undo)

--- src/GestionPedidos.java
+++ src/GestionPedidos.java
@@ -10,3 +10,27 @@ public class GestionPedidos {
         pilaPrincipal = new PilaPizza();
         pilaSecundaria = new PilaPizza();
     }
+    public void registrarPizza(Pizza pizza) {
+
+        pilaPrincipal.push(pizza);
+
+        pilaSecundaria.clear();
+
+        System.out.println("Pizza registrada correctamente.");
+    }
+
+    public void deshacer() {
+
+        if (pilaPrincipal.isEmpty()) {
+
+            System.out.println("No hay pedidos para deshacer.");
+            return;
+        }
+
+        Pizza pizza = pilaPrincipal.pop();
+
+        pilaSecundaria.push(pizza);
+
+        System.out.println("Pedido deshecho:");
+        System.out.println(pizza);
+    }
